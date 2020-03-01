5fd5d7a0e82ceef711dd4cd08d44aab626613fd2
Jordan <jordan.wallace.williams@gmail.com>
2020-03-01 13:33:57 -0700

    Weeks 4 and 5

--- spec/models/aroc_week_5_spec.rb
+++ spec/models/aroc_week_5_spec.rb
@@ -20,17 +20,18 @@ describe 'ActiveRecord Obstacle Course, Week 5' do
     expected_result = ['Abercrombie', 'Banana Republic', 'Calvin Klein', 'Dickies', 'Eddie Bauer', 'Fox', 'Giorgio Armani', 'Izod', 'J.crew']
 
     # ----------------------- Using Ruby -------------------------
-    items = Item.all
+    # items = Item.all
 
-    ordered_items = items.map do |item|
-      item if item.orders.present?
-    end.compact
+    # ordered_items = items.map do |item|
+    #   item if item.orders.present?
+    # end.compact
 
-    ordered_items_names = ordered_items.map(&:name)
-    ordered_items_names.sort
+    # ordered_items_names = ordered_items.map(&:name)
+    # ordered_items_names.sort
     # ------------------------------------------------------------
 
     # ------------------ ActiveRecord Solution ----------------------
+      ordered_items_names = Item.joins(:order_items).where("order_items.item_id").distinct.order(:name).pluck(:name)
     # Solution goes here
     # When you find a solution, experiment with adjusting your method chaining
     # Which ones are you able to switch around without relying on Ruby's Enumerable methods?
@@ -41,7 +42,7 @@ describe 'ActiveRecord Obstacle Course, Week 5' do
     expect(ordered_items_names).to_not include(unordered_items)
   end
 
-  xit '27. returns a table of information for all users orders' do
+  it '27. returns a table of information for all users orders' do
     custom_results = [@user_3, @user_1, @user_2]
 
     # using a single ActiveRecord call, fetch a joined object that mimics the
@@ -53,7 +54,12 @@ describe 'ActiveRecord Obstacle Course, Week 5' do
     # Zoolander      |         6
 
     # ------------------ ActiveRecord Solution ----------------------
-    custom_results = []
+    custom_results = User.select('users.name, count(orders.user_id) AS total_order_count')
+                          .joins(:orders)
+                          .where('orders.user_id')
+                          .group('users.name')
+                          .order('total_order_count')
+
     # ---------------------------------------------------------------
 
     expect(custom_results[0].name).to eq(@user_3.name)
